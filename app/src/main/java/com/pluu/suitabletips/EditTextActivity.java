60d52fcd3ade0e1f067d4645984b4b0c72a121ca
pluulove <pluulove@gmail.com>
2015-07-30 15:08:05 +0900

    Sample Edit

--- app/src/main/java/com/pluu/suitabletips/EditTextActivity.java
+++ app/src/main/java/com/pluu/suitabletips/EditTextActivity.java
@@ -1,12 +1,12 @@
 package com.pluu.suitabletips;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class EditTextActivity extends AppCompatActivity {
 
@@ -20,11 +20,18 @@ public class EditTextActivity extends AppCompatActivity {
 			@Override
 			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 				if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-					Toast.makeText(EditTextActivity.this, "Click Search", Toast.LENGTH_SHORT).show();
+					alertDialog("Click Search Action");
 				}
 				return false;
 			}
 		});
+	}
+
+	private void alertDialog(CharSequence msg) {
+		new AlertDialog.Builder(this)
+			.setMessage(msg)
+			.setPositiveButton(android.R.string.ok, null)
+			.show();
 	}
 
 }
